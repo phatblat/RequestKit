7af956905b58eb683c51ec074c1f35c5968199a2
Ben Chatelain <ben@octop.ad>
2016-06-18 22:58:48 -0600

    Workaround to CharacterSet removal crash
    rdar://26880260

--- RequestKit/String+Additions.swift
+++ RequestKit/String+Additions.swift
@@ -8,7 +8,8 @@ public extension String {
     func urlEncodedString() -> String? {
         let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
         let subDelimitersToEncode = "!$&'()*+,;="
-        var characterSet = CharacterSet.urlQueryAllowed
+        // Using CharacterSet.urlQueryAllowed crashes - http://openradar.appspot.com/26880260
+        var characterSet = NSCharacterSet.urlQueryAllowed()
         characterSet.remove(charactersIn: generalDelimitersToEncode + subDelimitersToEncode)
         return addingPercentEncoding(withAllowedCharacters: characterSet)
     }
